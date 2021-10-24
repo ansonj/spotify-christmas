d678eac170c17149d85fe7fb31f3e993a92c445a
Anson Jablinski <a@ansonj.org>
2021-10-24 09:43:51 -0500

    Stub run and spotify_christmas files

---
+++ bin/spotify_christmas.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Primary module, containing the interface and business logic
+module SpotifyChristmas
+  def self.run(sqlite_path)
+    puts "Running with sqlite at '#{sqlite_path}'"
+  end
+end
---
+++ run.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative 'bin/spotify_christmas'
+
+SpotifyChristmas.run ARGV.first
